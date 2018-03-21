d4cebbe4b1a08c5d128060f3a890ba358fb41b67
aleesham <avmoran@wustl.edu>
2018-03-21 10:13:51 -0500

    added numbers to TODOs

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ def index():
     movie = get_random_movie()
 
     # build the response string
-    content = "<h1>Movie of the Day</h1>"
+    content = "<h1>Today's Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
     content += "</ul>"
 
-    # TODO: pick another random movie, and display it under
+    # TODO 3: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
-
     return content
 
 def get_random_movie():
-    # TODO: make a list with at least 5 movie titles
-    # TODO: randomly choose one of the movies, and return it
+    # TODO 1: make a list with at least 5 movie titles
+    # TODO 2: randomly choose one of the movies, and return it
     return "The Big Lebowski"
 
 
